0c6b43785f70c09ceac26a6d893d58c6774c88c4
pconnors@uwo.ca <124845606+PconnorsUWO@users.noreply.github.com>
2025-05-08 16:32:44 -0400

    Working Prototype 1

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
-const repo = 'https://github.com/PconnorsUWO/Barcode_Reader_V2'; // change this to your repo name
+const repo = 'Barcode_Reader_V2';
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   output: 'export',
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  basePath: isProduction ? `/${repo}` : '',
+  assetPrefix: isProduction ? `/${repo}/` : '',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
 };
